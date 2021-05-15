7530daa4dbdeb3a78a4f06c6b385051e97dfbe32
Nikola Matošić <nikola.matosic2000@gmail.com>
2021-05-15 17:48:01 +0200

    Inicijalni commit .

---
+++ ProjektniZadatak/src/main/java/com/tim15/ProjektniZadatak/ProjektniZadatakApplication.java
@@ -0,0 +1,13 @@
+package com.tim15.ProjektniZadatak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektniZadatakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektniZadatakApplication.class, args);
+	}
+
+}
