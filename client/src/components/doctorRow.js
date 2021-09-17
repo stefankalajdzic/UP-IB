d2e456a6eaf0ce9bdb90372b131a22148ae5c046
stefanjelkic00 <stefankalajdzic95@gmail.com>
2021-09-17 20:54:12 +0200

    ui red doktora foramtiran

--- client/src/components/doctorRow.js
+++ client/src/components/doctorRow.js
@@ -1,19 +1,15 @@
-import {Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import React from "react";
 
-
 const doctorRow = (props) => {
-
-    return (
-        <tr>
-            <td>{props.id}</td>
-            <td>{props.name}</td>
-            <td>{props.email}</td>
-            <td>{props.phoneNumber}</td>
-        </tr>
-    )
-
-
-}
+  return (
+    <tr>
+      <td>{props.id}</td>
+      <td>{props.name}</td>
+      <td>{props.email}</td>
+      <td>{props.phoneNumber}</td>
+    </tr>
+  );
+};
 
 export default doctorRow;
